2f6d45fecde2b62587ddcdc553cd797c1bc98017
Hawkjeet <jeetmulani2006@gmail.com>
2025-07-22 06:13:06 +0530

    SplashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.foodhub_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.foodhub_app"
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.core.splashscreen)
 }
